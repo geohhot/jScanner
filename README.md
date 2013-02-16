90ed4119fa1d225ccb5d3da0ebeefb7dff8d87fa
Gevorg Hindoyan <ghindoyan@gmail.com>
2013-02-16 23:37:09 +0400

    Added threads and -h argument

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ jScanner
 ========
 
 Java port scanner. That was task on my java seminar, so I made repository, and will try to make it best. The main point is to use threads to make scanner faster. 
+
+Parameters
+==========
+
+Use <b>-h</b/> or <b>--host</b> to set host to scan. 
--- src/jScanner.java
+++ src/jScanner.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 
 
 public class jScanner {
+	static int openPorts = 0;
+	static InetAddress address;
 	public static void main(String[] args) {
 		
 		String host = null;
@@ -19,7 +21,13 @@ public class jScanner {
 		while (argsIn.hasNext()) {
 			String n = argsIn.next();
 			if (n.equals("-h") || n.equals("--host")) {
-				host = argsIn.next();
+				if(argsIn.hasNext()) {
+					host = argsIn.next();
+				}
+				else {
+					System.out.println("Invalid arguments.");
+					System.exit(0);
+				}
 			}
 		}
 		
@@ -30,23 +38,26 @@ public class jScanner {
 		
 		try {
 			Date startDate = new Date();
-			InetAddress address = InetAddress.getByName(host);
+			address = InetAddress.getByName(host);
 			
 			System.out.println("Scanning "+address.getHostName()+" ["+address.getHostAddress()+"].");
 			
-			int openPorts = 0;
 			int portsToScan = 7000;
 			
 			for (int i=1;i<portsToScan;i++) {
-				try {
-					Socket s = new Socket(address, i);
-					System.out.println("* Port "+i+" is up.");
-					openPorts ++;
-					s.close();
-				}
-				catch (Exception e) {
-					continue;
-				}
+				final int port = i;
+				Thread scan = new Thread(new Runnable() {
+					public void run() {
+						try {
+							Socket s = new Socket(address, port);
+							System.out.println("* Port "+port+" is up.");
+							openPorts ++;
+							s.close();
+						}
+						catch (Exception e) {}
+					}
+				});
+				scan.start();
 			}
 			Date endDate = new Date();
 			long t = endDate.getTime() - startDate.getTime();
